df82e85e4ba621fb0d616b8f38c833ce47203582
Rishi <rishikm215@gmail.com>
2024-12-01 19:36:51 +0530

    some basic codes

---
+++ Basics/sum_of_two_nums.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int  main()
+{
+    int num1,num2;
+    cout << "Enter Frst Number : ";
+    cin >> num1;
+    cout << "Enter Second Number : ";
+    cin >> num2;
+
+    cout << "Sum of the numbers is :" << num1+num2 << endl;
+    return 0;
+}
---
+++ Basics/traingle_pattern_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i,j,row;
+    cout <<"Enter Number of Rows :";
+    cin >> row;
+    for(i=0;i<row;i++)
+    {
+        for (j=0;j<=i;j++)
+        {
+            cout << " * "; 
+        }
+        cout << "\n";
+    }
+}
