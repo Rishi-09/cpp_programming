e57faabfc99db7edfea72283f65b9493250b5efb
Rishi <rishikm215@gmail.com>
2024-12-06 23:09:56 +0530

    inverted triangle

---
+++ Basics/patterns/inverted_triangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row,count=1;
+    cout << "Enter the Number of Rows :";
+    cin >> row;
+    for(int i=1;i<=row;i++)
+    {   for(int k=1;k<=i;k++)
+        {
+            cout << " ";
+        }
+        for(int j=row;j>=i;j--)
+        {
+            cout << i ;
+        }
+        
+        cout << endl ;
+    }
+}
