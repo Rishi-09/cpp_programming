3a53bc9113d082c3e27223a710bd7eee7d2d2b64
Rishi <rishikm215@gmail.com>
2024-12-27 00:52:09 +0530

    single-elemnet in a vector

---
+++ DSA/vectors/single_num.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int SingleNum(vector <int> &num)
+{ 
+    int ans = 0;
+    for(int i:num)
+    {
+        ans ^=i;       
+    }
+    return ans;
+}
+int main()
+{
+	vector <int> arr;
+    int n;
+    cout << "enter number of elements :";
+    cin >> n;
+	for ( int i =0 ; i< n ;i++)
+	{	cout << "Enter Element at index " << i << " : " ;
+		int val;
+		//cin >> val;
+		arr.push_back( cin >>  val);
+		
+	}
+	cout << "Entered Array :";
+	for ( int i =0 ; i< n ;i++)
+	{
+		cout << arr[i] <<  " ";
+		
+	}	
+	cout << endl;
+    cout << SingleNum(arr) << endl ;
+}
