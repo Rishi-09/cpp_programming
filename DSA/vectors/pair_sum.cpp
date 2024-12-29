9abe3feeb83a7e110fae56416a8893e742de0922
Rishi <rishikm215@gmail.com>
2024-12-29 12:45:57 +0530

    working code

--- DSA/vectors/pair_sum.cpp
+++ DSA/vectors/pair_sum.cpp
@@ -1,32 +1,52 @@
 #include <iostream>
-#include <vectors>
+#include <vector>
 using namespace std;
-int pair_sum(vector <int>& arr ,int target)
-{
-  for (int i =0 ;i < arr.size() ; i++)
+vector<int> pair_sum(vector <int>& arr ,int target)
+{ 
+  vector <int> result;
+  int left = 0,right = arr.size()-1;
+  while (left < right)
     {
-      if (arr[i]+arr[i+1]==target)
-        return i;
+      
+        if ( (arr[left]+arr[right]) > target )
+        {
+          right--;
+        }
+        else if (arr[left]+arr[right]<target)
+        {
+          left++;
+        }
+        else  
+        {
+          result.push_back(left);
+          result.push_back(right);
+          return result;
+        }
     }
-  return -1;
+  return result;
 }
+
 int main()
 {
-  vector <int> arr,size;
+  vector <int> arr;
+  int size;
   cout << "Enter the number of elements : " ;
   cin >> size;
   for (int i = 0 ;i < size ;i++)
     {
       cout << "Enter the Element at index " << i << " : ";
-      cin >> arr[i];
+      int val;
+      cin >> val;
+      arr.push_back(val);
     }
   int target;
   cout << "Enter the target sum : ";
   cin >> target;
-  res = pair_sum(arr , target);
-  if (res==-1)
-    cout << "Pair Sum not found : " << endl;
+  
+  if (pair_sum(arr,target).size()==0)
+    cout << "Pair Sum not found " << endl;
   else
-    cout << "pair is:" << res << "," << res+1 << endl;
+    cout << pair_sum(arr,target)[0] << "," << pair_sum(arr,target)[1];
+  cout << endl;
   return 0;
 }
