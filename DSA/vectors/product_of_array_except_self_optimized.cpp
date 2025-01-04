d57141365761ab471874758606f6c9a2108a3882
Rishi <rishikm215@gmail.com>
2025-01-04 18:30:17 +0530

    product_of_array_except_self using brute force approach

---
+++ DSA/vectors/product_of_array_except_self_optimized.cpp
@@ -0,0 +1,36 @@
+ #include <iostream>
+ #include <vector>
+ using namespace std;
+ vector<int> productExceptSelf(vector<int>& nums) {
+        vector <int> ans(nums.size(),1);
+        for(int i = 1;i<nums.size();i++)
+        {
+            ans[i]=ans[i-1]*nums[i-1];    
+        }
+        int suffix=1;
+        for(int i=nums.size()-2;i>=0;i--)
+        {   suffix*=nums[i+1];
+            ans[i]*=suffix;
+        }
+            return ans;
+    }
+int main()
+{
+    vector<int> arr;
+    int size;
+    cout << "Enter number of elements in array : ";
+    cin >> size;
+    for (int i = 0; i < size; i++)
+    {
+        int val;
+        cout << "Enter element at index " << i << " :";
+        cin >> val;
+        arr.push_back(val);
+    }
+    vector <int> res = productExceptSelf(arr);
+    for (int i = 0; i < size; i++)
+    {
+        cout << res[i] << " ";
+    }
+    cout << endl;
+}
